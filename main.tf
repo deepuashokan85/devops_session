a5b8f1c33eb347bf84f3b9a37fe3c4846310e245
narendrabr <narendrabr@github.com>
2019-08-24 07:56:18 +0530

    My Second Commit on 23 Aug

---
+++ main.tf
@@ -0,0 +1,11 @@
+# First Terraform File
+
+provider "aws" {
+	profile = "default"
+	region = "us-east-2"
+	}
+
+resource "aws_instance" "example" {
+	instance_type = "t2.micro"
+	ami = "ami-0520e698dd500b1d1"
+	}
